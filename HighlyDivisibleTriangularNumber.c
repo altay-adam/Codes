1559af5ee7974756e12d65c238b162e940c8420f
Altay Adam <62152831+altay-adam@users.noreply.github.com>
2020-08-25 12:07:15 +0300

    Create HighlyDivisibleTriangularNumber.c

---
+++ HighlyDivisibleTriangularNumber.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+int main() {
+	int divisorCounter = 0;
+	int Value = 0;
+	int number = 1;
+	int temp;  //It's temporary variable. I'm gonna equal it the number temporarily.
+	while (true) {
+		Value += number;
+		temp = number;
+		number = 1;
+		while (number<=Value){
+			if (Value % number == 0) {
+				divisorCounter++;
+			}
+			number++;
+		}
+		number = temp;
+		if (divisorCounter > 500) {
+			printf("%d", Value); break;
+		}
+		number++;
+		divisorCounter = 0;
+	}
+	
+	return 0;
+}
