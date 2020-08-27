394c8590dc0a5e1717275880e7c46af9909207fb
Altay Adam <62152831+altay-adam@users.noreply.github.com>
2020-08-28 01:38:09 +0300

    Update HighlyDivisibleTriangularNumber.cpp

--- HighlyDivisibleTriangularNumber.cpp
+++ HighlyDivisibleTriangularNumber.cpp
@@ -1,28 +1,36 @@
 #include <iostream>
 using namespace std;
 int main() {
-
-	int divisorCounter = 0;
 	int Value = 0;
-	int number = 1;
-	int temp;  //It's temporary variable. I'm gonna equal it to the number temporarily.
+	int tourCounter = 1;
+	int product = 1;
 	while (true) {
-		Value += number;
-		temp = number;
-		number = 1;
-		while (number<=Value){
-			if (Value % number == 0) {
-				divisorCounter++;
+		Value += tourCounter; //Everytime, topCounter is added to Value to find triangle numbers.
+		int number = 2;
+		int temp = Value; //Temporary variable
+		int counter = 0;
+		while (temp >= 1) {  //In this loop, we're gonna find every divisors that can divide the triangle numbers sequence.
+			if (temp % number == 0) {	//Here, we divide temp to the number until our remainder equals 0. This is because to find multipliers of the temp(Value).
+				temp = temp / number;
+				counter++;
 			}
-			number++;
+			else {
+				number++;		// If the number can't divide the temp without remainder, number increases 1.
+				if (counter > 0) {
+					product *= (counter + 1);  //Product equals that "(counter+1)*product". This is because to find multipliers of the temp(Value).
+				}
+				counter = 0;
+				if (temp == 1) {
+					break;
+				}
+			}
 		}
-		number = temp;
-		if (divisorCounter > 500) {
+		if (product > 500) {
 			cout << Value; break;
 		}
-		number++;
-		divisorCounter = 0;
+		tourCounter++;		//Every end of the loop tourCounter increases 1 to find squence of the triangle numbers.(1,3,6,10,15,21,28....)
+		product = 1;     //Every end of the loop we equal product to 1.
 	}
-	
+
 	return 0;
 }
